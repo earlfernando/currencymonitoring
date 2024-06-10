33034a644c5f43f550f9839ddc2cd0d1d7ad58b2
Earl Fernando <earl@earls-mbp.home>
2024-06-10 18:33:27 +0200

    Optimize sampling function to use sample rate and duration efficiently
    Earlier the program logged the average of the currency rates to the
    logging file at the end of the program, but we would like to add
    the logs at the end of every  60 seconds.

--- currencyMonitor.py
+++ currencyMonitor.py
@@ -1,9 +1,11 @@
 import logging
 from time import sleep
-import random
+#import random
 import requests
 import numpy as np
 import argparse
+import datetime
+import sys
 
 logger = None
 
@@ -26,7 +28,7 @@ def getCurrencyConversionRates(orig_currency: str, dest_currency: str, api_key: 
     data = response.json()
     if response.status_code == 200:
         conversion_rate = data['conversion_rates'][dest_currency]
-        logging.info(f"Conversion rate from {orig_currency} to {dest_currency}: {conversion_rate}")
+        logger.info(f"Conversion rate from {orig_currency} to {dest_currency}: {conversion_rate}")
 
     return conversion_rate
 
@@ -34,31 +36,42 @@ def main():
     # Create the parser
     parser = argparse.ArgumentParser(description="A simple program to show the currency rates", add_help=True)
     parser.add_argument('-f', '--log_file', type=str, default = "currencymonitor.log", help='Logging File name')
-    parser.add_argument('-b', '--base_currency', type=str, default = "USD", help='Base currency to transfer from')
-    parser.add_argument('-d', '--dest_currency', type=str, default = "EUR", help='Destination currency to transfer to')
+    parser.add_argument('-b', '--base_currency', type=str, default = "USD", help='Base currency to exchange from')
+    parser.add_argument('-d', '--dest_currency', type=str, default = "EUR", help='Destination currency to exchange to')
     parser.add_argument('-a', '--api_key', type=str, default = "e811708c87af40b8525b1aa8", help='API key to https://app.exchangerate-api.com/dashboard')
-    parser.add_argument('--sample_rate', type=int, default = "2", help='Sample rate to get the exchange currency values')
-    parser.add_argument('--duration', type=int, default = "5", help='Overall duration for sampling the exchange rate')
+    parser.add_argument('--sample_rate', type=int, default = "5", help='Sample rate to get the exchange currency values in seconds')
+    parser.add_argument('--duration', type=int, default = "60", help='Overall duration for sampling the exchange rate in seconds')
     # Parse the arguments
     args = parser.parse_args()
     log_file = args.log_file
+    global logger
     logger = get_logger(log_file)
-
     sample_rate_sec = args.sample_rate
     sample_interval_sec = args.duration
     api_key = args.api_key
     orig_currency = args.base_currency
     dest_currency = args.dest_currency
 
+    if sample_interval_sec <= sample_rate_sec:
+        logger.error("Duration shorter that sample rate")
+        sys.exit(1)
+
     currency_rates = np.array([])
+    average_logger_bool = False
+    timer = 0 
     while sample_interval_sec > 0:
-        logging.debug("Sampling API")
+        logger.debug("Sampling API")
+        if timer >= 60:
+            average_logger_bool = True
+            logger.info(f"Average of over 60 seconds {np.average(currency_rates)}")
+            timer = 0
+        sleep(sample_rate_sec)
         #currency_rates = np.append(currency_rates,random.random())
         currency_rates = np.append(currency_rates,getCurrencyConversionRates(orig_currency, dest_currency, api_key))
-        #currency_rates.append(getCurrencyConversionRates(orig_currency, dest_currency, api_key))
-        sleep(sample_rate_sec)
+        timer = timer + sample_rate_sec
         sample_interval_sec = sample_interval_sec - sample_rate_sec
-    logger.info(f"Average of over 60 seconds {np.average(currency_rates)}")
+    if not average_logger_bool:
+        logger.info(f"Average exchange rate over {args.duration} seconds is {np.average(currency_rates)}")
 
 if __name__ == '__main__':
     main()
