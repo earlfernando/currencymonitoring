5fbc182b763ac9aff98d81c0a480f8cd52be7e0f
Earl Fernando <earl@earls-mbp.home>
2024-06-10 17:46:21 +0200

    Add base_currency, dest_currency, api_key , sample_rate and duration args to argparser
    In this commit, we are adding the following args to the arg parser
    
    1. Base currency : Currency we are exchanging from
    2. Destination currency : Currency we are exchanging to
    3. api_key : Api key to  https://app.exchangerate-api.com/dashboard
    4. sample_rate : How often we should request the api for exchange rate
    5. duration : For how long we should keep sampling for exchange rate

--- currencyMonitor.py
+++ currencyMonitor.py
@@ -34,17 +34,21 @@ def main():
     # Create the parser
     parser = argparse.ArgumentParser(description="A simple program to show the currency rates", add_help=True)
     parser.add_argument('-f', '--log_file', type=str, default = "currencymonitor.log", help='Logging File name')
-    
+    parser.add_argument('-b', '--base_currency', type=str, default = "USD", help='Base currency to transfer from')
+    parser.add_argument('-d', '--dest_currency', type=str, default = "EUR", help='Destination currency to transfer to')
+    parser.add_argument('-a', '--api_key', type=str, default = "e811708c87af40b8525b1aa8", help='API key to https://app.exchangerate-api.com/dashboard')
+    parser.add_argument('--sample_rate', type=int, default = "2", help='Sample rate to get the exchange currency values')
+    parser.add_argument('--duration', type=int, default = "5", help='Overall duration for sampling the exchange rate')
     # Parse the arguments
     args = parser.parse_args()
     log_file = args.log_file
     logger = get_logger(log_file)
 
-    sample_rate_sec = 1
-    sample_interval_sec = 2
-    api_key = 'e811708c87af40b8525b1aa8'
-    orig_currency = 'USD'
-    dest_currency = 'EUR'
+    sample_rate_sec = args.sample_rate
+    sample_interval_sec = args.duration
+    api_key = args.api_key
+    orig_currency = args.base_currency
+    dest_currency = args.dest_currency
 
     currency_rates = np.array([])
     while sample_interval_sec > 0:
